5d075ee43d9531ec0c4dc27f16517e6f8fbf09bd
Brotot Biswas <brototi.biswas1@gmail.com>
2020-08-05 23:05:27 -0400

    Hooks to setup and teardown driver

--- src/test/java/pageObjects/Hooks.java
+++ src/test/java/pageObjects/Hooks.java
@@ -1,4 +1,42 @@
 package pageObjects;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Hooks {
+    public static WebDriver driver;
+
+    /**
+     * Set driver to browser
+     * Default browser: Chrome
+     */
+    @Before
+    public void setDriver() {
+        String browser = System.getProperty("browser","chrome");
+
+        switch(browser) {
+            case "firefox" :
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "chrome" :
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+        }
+    }
+
+    /**
+     * Close all the tabs and quit browser
+     */
+    @After
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.close();
+        driver.quit();
+    }
 }
