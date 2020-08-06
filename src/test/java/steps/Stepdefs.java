e23c1715ff87f56c273b7529a7df58666e8a1022
Brotot Biswas <brototi.biswas1@gmail.com>
2020-08-05 23:04:43 -0400

    Add steps to navigate and search

--- src/test/java/steps/Stepdefs.java
+++ src/test/java/steps/Stepdefs.java
@@ -4,27 +4,30 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import org.openqa.selenium.WebDriver;
+import pageObjects.Homepage;
 
 public class Stepdefs {
-    WebDriver driver;
+    Homepage homepage;
 
     @Given("User navigates to {string}")
     public void userNavigatesTo(String url) {
-
+        homepage = new Homepage(url);
+        homepage.openBrowser(url);
     }
 
     @And("Searches for a {string}")
     public void searchesForA(String term) {
-        
+        System.out.println("search");
+        homepage.search(term);
     }
 
     @Then("User clicks on the third result")
     public void userClicksOnTheThirdResult() {
-
+        System.out.println("third");
     }
 
     @And("User should be able to open in a new tab")
     public void userShouldBeAbleToOpenInANewTab() {
+        System.out.println("new tab");
     }
 }
